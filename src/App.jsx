361164a903fde0acdc8f5a79f8f9c2f6dea5786c
Mehrshad Khatibi <khatibimehrshad24@gmail.com>
2024-11-23 16:51:41 +0330

    mobile size status card's size fixed

--- src/App.jsx
+++ src/App.jsx
@@ -7,15 +7,15 @@ export default function App() {
   return (
     <div className="flex flex-wrap justify-evenly p-5 lg:gap-5 md:gap-x-2 mb-20 lg:gap-y-10 md:gap-y-3 gap-y-5 dark:bg-darkPrimary dark:text-white rounded-md  bg-primary sm:w-[90%] w-full text-fontColor ">
       <ThreeCards />
-      <div className="lg:flex-[0_0_96%] flex-[0_0_100%] dark:bg-darkSecondary dark:text-white rounded-md  bg-secondary  text-fontColor gap-2 p-5">
+      <div className="lg:flex-[0_0_96%] flex-[0_0_100%] dark:bg-darkSecondary dark:text-white rounded-md  bg-secondary  text-fontColor gap-2 lg:p-5 p-2">
         <h1 className="p-2">پر فروش ترین کالاها</h1>
         <LineChart />
       </div>
-      <div className="lg:flex-[0_0_46%] flex-[0_0_100%] dark:bg-darkSecondary dark:text-white rounded-md  bg-secondary  text-fontColor gap-2 p-5">
+      <div className="lg:flex-[0_0_46%] flex-[0_0_100%] dark:bg-darkSecondary dark:text-white rounded-md  bg-secondary  text-fontColor gap-2 lg:p-5 p-2">
         <h1 className="p-2">فروش هفته گذشته</h1>
         <BarChart />
       </div>
-      <div className="lg:flex-[0_0_46%] flex-[0_0_100%] dark:bg-darkSecondary dark:text-white rounded-md  bg-secondary  text-fontColor gap-2 p-5 ">
+      <div className="lg:flex-[0_0_46%] flex-[0_0_100%] dark:bg-darkSecondary dark:text-white rounded-md  bg-secondary  text-fontColor gap-2 lg:p-5 p-2 ">
         <h1 className="p-2">تعداد کاربران بر اساس سن</h1>
         <PieChart />
       </div>
